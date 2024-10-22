728b45fc19e7e36999b82552e3a3395b959dd65e
Ohzen831 <113336662+Ohzen831@users.noreply.github.com>
2024-10-22 14:43:27 +0800

    Create home_page.dart

---
+++ home_page.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  final List<String> stories;
+  final List<String> posts;
+
+  HomeScreen({required this.stories, required this.posts});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late List<Color> _storyColors;
+
+  @override
+  void initState() {
+    super.initState();
+    _storyColors = List<Color>.filled(widget.stories.length, Colors.red);
+  }
+
+  void _onStoryTap(int index) {
+    setState(() {
+      _storyColors[index] =
+          _storyColors[index] == Colors.red ? Colors.blue : Colors.red;
+    });
+    print("Tapped on: ${widget.stories[index]}");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(56.0),
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 138, 77, 179),
+                Color.fromARGB(255, 207, 149, 94),
+                Color.fromARGB(255, 238, 177, 212),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: AppBar(
+            backgroundColor: Colors.transparent,
+            title: Text("Insta-gramy"),
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 80,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: widget.stories.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(4),
+                  child: GestureDetector(
+                    onTap: () => _onStoryTap(index),
+                    child: Container(
+                      width: 80,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: _storyColors[index],
+                      ),
+                      child: Center(
+                        child: Text(
+                          widget.stories[index],
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: widget.posts.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(top: 2, bottom: 4),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        widget.posts.removeAt(index);
+                      });
+                    },
+                    child: Container(
+                      height: 200,
+                      color: Colors.red,
+                      child: Center(
+                        child: Text(widget.posts[index]),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
